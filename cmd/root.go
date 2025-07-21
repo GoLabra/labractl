39dd87645af696d26af64cfa19afd642813aa04a
Dan P <podinacristi@gmail.com>
2025-07-21 18:43:12 +0300

    Fix debug flag initialization

--- cmd/root.go
+++ cmd/root.go
@@ -15,6 +15,11 @@ var rootCmd = &cobra.Command{
 	Use:   "labractl",
 	Short: "CLI tool for LabraGo",
 	Long:  `CLI tool to create and start a LabraGo project`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		envDebug := strings.ToLower(os.Getenv("LABRA_DEBUG"))
+		enableDebug := debug || envDebug == "1" || envDebug == "true"
+		log.Init(enableDebug)
+	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -25,10 +30,6 @@ var debug bool
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	envDebug := strings.ToLower(os.Getenv("LABRA_DEBUG"))
-	enableDebug := debug || envDebug == "1" || envDebug == "true"
-	log.Init(enableDebug)
-
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
